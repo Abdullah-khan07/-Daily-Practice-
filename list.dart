2da63aec621a6ca87bcda90104666ce92ec5fa96
Abdullah-khan07 <abdullahahmedkhan072@gmail.com>
2024-12-10 01:47:11 -0800

    Add Method

--- list.dart
+++ list.dart
@@ -1,10 +1,5 @@
 void main() {
-  List< int> student = [
-   5 ,
-   89,
- 58 ,
-   2 ,
-  ];
-  student.sort();
+  List<int> student = [45, 78, 12, 56, 15, 19];
+  student.add(99);
   print(student);
 }
