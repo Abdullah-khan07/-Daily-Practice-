17f790ac8545fdb070e609b7cf2db70e1b0a3836
Abdullah-khan07 <abdullahahmedkhan072@gmail.com>
2024-12-15 18:31:33 -0800

    Answer is {Abdullah: 1, Archiyan: 5, rizwan: 40}

--- map.dart
+++ map.dart
@@ -1,8 +1,5 @@
 void main() {
   Map<String, dynamic> kasmir = {"Abdullah": 1, "Archiyan": 5};
-  if (kasmir.containsKey("Abdullah")) {
-    print("code is run");
-  } else {
-    print("code is not run");
-  }
+  kasmir.putIfAbsent("rizwan", () => 40);
+  print(kasmir);
 }
