44a2536daf2447f6147eda10cd1e8d69e737d935
Abdullah-khan07 <abdullahahmedkhan072@gmail.com>
2024-12-21 19:10:37 -0800

    pre& post increment & decrement

--- increment.dart
+++ increment.dart
@@ -1,5 +1,10 @@
 void main() {
-  var abc = 0;
-  var a = ++abc;
-  print(a);
+  var a = 0;
+  var abc = a++;
+  var xyz = ++abc + --a;
+  var zxy = --abc - ++xyz;
+  print("A value $a");
+  print("B value $abc");
+  print(" xyz value$xyz ");
+  print("  zxy value $zxy");
 }
