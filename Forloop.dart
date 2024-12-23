7fde31bdd291ffea17aa0bcb2f78505a107a7d7f
Abdullah-khan07 <abdullahahmedkhan072@gmail.com>
2024-12-22 20:12:28 -0800

    proper table

--- Forloop.dart
+++ Forloop.dart
@@ -3,7 +3,7 @@ import 'dart:io';
 void main() {
   stdout.write("Enter the number");
   int number = int.parse(stdin.readLineSync()!);
-  for (var i = 0; i <= 10; i++) {
-    print(i * number);
+  for (var i = 1; i <= 10; i++) {
+    print( "$number x $i = ${number*i}");
   }
 }
