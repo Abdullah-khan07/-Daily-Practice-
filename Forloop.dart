a09284dce01b62478654095e42fd0fa157f081f9
Abdullah-khan07 <abdullahahmedkhan072@gmail.com>
2024-12-23 17:32:20 -0800

    even & odd method

--- Forloop.dart
+++ Forloop.dart
@@ -1,9 +1,16 @@
 import 'dart:io';
 
 void main() {
-  stdout.write("Enter the Number =");
-  num number = int.parse(stdin.readLineSync()!);
-  for (var i = 1; i <= 10; i++) {
-    print("$number x = $i ${number * i}");
+  List number = [4, 9, 2, 4, 8, 6, 7];
+  List evennumber = [];
+  List oddnumber = [];
+  for (var i = 0; i < number.length; i++) {
+    if (number[i] % 2 == 0) {
+      evennumber.add(i);
+    } else {
+      oddnumber.add(i);
+    }
   }
+  print("Even Number = $evennumber");
+  print("oddnumber = $oddnumber");
 }
