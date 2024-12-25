2f1cca0c3555a728e6327d90a9a18fe714bb5bfe
Abdullah-khan07 <abdullahahmedkhan072@gmail.com>
2024-12-24 19:29:27 -0800

    find the largest number

--- Loopchallanges.dart
+++ Loopchallanges.dart
@@ -1,9 +1,8 @@
-void main (){
-  String str = "flutter";
-String reversed = "";
-for (int i = str.length - 1; i >= 0; i--) {
-  reversed += str[i];
-}
-print("Reversed String: $reversed");
-
+void main() {
+  //Write a for loop to find the largest number in a list without using built-in functions.
+  for (var i = 1; i <= 200; i++) {
+    if (i % 2 == 0) ;
+    print(i);
+  }
+  
 }
