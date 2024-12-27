77b0be5d43e22dc130290bff76cc9314ab7cb076
Abdullah-khan07 <abdullahahmedkhan072@gmail.com>
2024-12-27 02:21:05 -0800

    Nasted loop star method

--- Nested-loop.dart
+++ Nested-loop.dart
@@ -1,8 +1,10 @@
+import 'dart:io';
+
 void main() {
-  for (int i = 1; i <= 5; i++) {
-    for (int j = 1; j <= 5; j++) {
-      print('$i * $j = ${i * j}');
+  for (var i = 1; i <= 5; i++) {
+    for (var j = 1; j <= i; j++) {
+      stdout.write("*");
     }
-    print(''); // Print a blank line after each row
+    print("");
   }
 }
