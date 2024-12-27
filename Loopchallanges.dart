22c8d7784241dff22d8fdcc16bbf6310357928fe
Abdullah-khan07 <abdullahahmedkhan072@gmail.com>
2024-12-27 15:05:21 -0800

    paractice

--- Loopchallanges.dart
+++ Loopchallanges.dart
@@ -1,23 +1,12 @@
 void main() {
-  List students = [
-    {
-      "name": "Rizwan",
-      "age": 23,
-      "isStudent": true,
-    },
-    {
-      "name": "Ahmed",
-      "age": 25,
-      "isStudent": true,
-    },
-    {
-      "name": "Ali",
-      "age": 15,
-      "isStudent": false,
-    },
+  List student = [
+    {"name": "abdullah", "class": "10", "age": "15"},
+    {"name": "ahmad", "class": 9, "age": 20},
+    {"name": "archiyan ", "class": 8, "age": 7}
   ];
 
-  for (int i = 0; i < students.length; i++) {
-    print(students[2]["isStudent"]);
-  }
+  // Kisi ek specific student ka data access karne ke liye index ka use karen
+  print(student[0]); // Pehle student ka pura data
+  // Ya phir specific field access karne ke liye
+  print(student[0]["name"]); // Pehle student ka naam
 }
--- map.dart
+++
@@ -1,5 +0,0 @@
-void main() {
-  Map<String, dynamic> kasmir = {"Abdullah": 1, "Archiyan": 5};
-  kasmir.putIfAbsent("rizwan", () => 40);
-  print(kasmir);
-}
