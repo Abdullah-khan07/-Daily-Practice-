b4b573e56ea009a9eda8cba39efd972731e5e848
Abdullah-khan07 <abdullahahmedkhan072@gmail.com>
2024-12-28 00:55:08 -0800

    paractice

--- whileloop.dart
+++ whileloop.dart
@@ -1,19 +1,17 @@
-import 'dart:io';
-
 void main() {
-  bool login = false;
-  while (login == false) {
-    stdout.write("Please input the email: ");
-    var email = stdin.readLineSync()!;
-    
-    stdout.write("Please input the password: ");
-    var password = stdin.readLineSync()!;
-    
-    if (email == "abdullah@gmail.com" && password == "5612") { // Removed the semicolon here
-      print("Login successful");
-      login = true;
+  // Student ke marks
+  int marks = 75;
+
+  // While loop ka use karke pass/fail check karna
+  while (marks >= 0) {
+    if (marks < 40) {
+      print("Student ke marks $marks hain - FAIL");
     } else {
-      print("Login failed");
+      print("Student ke marks $marks hain - PASS");
     }
+
+    // Agle student ke marks ke liye (example ke liye)
+    marks =
+        marks - 15; // Har bar 15 marks kam kar rahe hain demonstration ke liye
   }
 }
